bdea3c938f7f806c3c780217426c4c12c53a83bd
hexa- <mweinelt@gmail.com>
2013-02-14 06:28:57 +0100

    Plugin/nickname_keeper: Update to use OnNick-Event, also implement retry for WATCH if we try too early

--- plugins/nickname_keeper.py
+++ plugins/nickname_keeper.py
@@ -1,3 +1,4 @@
+from lib.EventHandler import EventHandler, Event
 from lib.Plugin import Plugin
 import random
 
@@ -8,6 +9,8 @@ class P(Plugin):
     
     # here be dragons
     def loaded(self):
+        self.events = EventHandler()
+        self.working = False
         print "nickname_keeper module loaded\n\tprimary=%s, secondary=%s" % (self.nickname, self.secondary_nickname)
         
     def onRaw(self, msg):
@@ -18,6 +21,7 @@ class P(Plugin):
             if (tok[1] == self.nickname):
                 self.send("NICK %s" % self.secondary_nickname)
                 self.send("WATCH +%s" % self.nickname)
+                self.working = True
                 print "[nickname_keeper] Nickname taken, putting it on the watchlist..."
             elif (tok[1] == self.secondary_nickname):
                 self.send("NICK %s%d" % (self.nickname, random.randrange(1,999)))
@@ -25,8 +29,31 @@ class P(Plugin):
         elif (msg.getType() == 601) or (msg.getType() == 605):
             # 601 THIS IS SOME WORKAROUND FOR UNREALIRCD MADNESS, WTF?!
             # 605: nick userid host time :is offline
-            tok = msg.tokenize()
+            tok = msg.getMessage().split()
             if (tok[1] == self.nickname):
                 print "[nickname_keeper] Our nickname was given up, changing now and clearing watchlist!"
                 self.send("NICK %s" % self.nickname)
                 self.send("WATCH -%s" % self.nickname)
+                self.working = False
+        elif (msg.getType() == 451):
+            # 451: WATCH :You have not registered
+            self.events.register("watch", 10000, self.nickname) # retry
+            print "[nickname_keeper] Tried to WATCH too early, retrying in 10s"
+            
+    def update(self, diff):
+        self.events.update(diff)
+        
+        while 1:
+            event = self.events.next()
+            if not event:
+                break
+            
+            if (event.getName() == "watch"):
+                if (self.working):
+                    self.send("WATCH +%s" % event.getBlob())
+                    
+    def onNick(self, user, newnick):
+        if (user[0] == self.nickname):
+            self.send("NICK %s" % self.nickname)
+            self.send("WATCH -%s" % self.nickname)
+            self.working = False
