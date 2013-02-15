f3a3916902c43aa7cd6e08e735d95f6db62225d4
hexa- <mweinelt@gmail.com>
2013-02-16 00:44:00 +0100

    Tests: Complete Line-Coverage for PluginSystem::process and Plugin

--- tests/plugins/test.py
+++ tests/plugins/test.py
@@ -26,3 +26,30 @@ class TestPlugin(Plugin):
         
     def onRaw(self, msg):
         self.lastEvent = msg
+        
+    def onCTCP(self, msg):
+        self.lastEvent = msg
+        
+    def onJoin(self, user, channel):
+        self.lastEvent = (user, channel)
+        
+    def onPart(self, user, channel, message):
+        self.lastEvent = (user, channel, message)
+        
+    def onQuit(self, user, message):
+        self.lastEvent = (user, message)
+        
+    def onTopic(self, user, channel, message):
+        self.lastEvent = (user, channel, message)
+
+    def onNick(self, user, newnick):
+        self.lastEvent = (user, newnick)
+        
+    def onMode(self, user, channel, mode):
+        self.lastEvent = (user, channel, mode)
+        
+    def onUserMode(self, nick, target, mode):
+        self.lastEvent = (nick, target, mode)
+        
+    def onKick(self, user, channel, victim, reason):
+        self.lastEvent = (user, channel, victim, reason)
--- tests/unittest_pluginsystem.py
+++ tests/unittest_pluginsystem.py
@@ -24,19 +24,89 @@ class testPluginSystem(unittest.TestCase):
         self.assertIsInstance(self.testPlugin, Plugin)
         
     def testHooksPrivmsg(self):
-        self.msg = Message(":GnoXter!gnoxter@local-954F9AB.clients.your-server.de PRIVMSG #chan :Momentan blocken sie das Pluginsystem  bzw. update()")
-        self.plugins.process(self.msg)
-        self.assertEqual(self.msg, self.testPlugin.getLastMessage())
+        msg = Message(":GnoXter!gnoxter@local-954F9AB.clients.your-server.de PRIVMSG #chan :Momentan blocken sie das Pluginsystem  bzw. update()")
+        self.plugins.process(msg)
+        self.assertEqual(msg, self.testPlugin.getLastMessage())
       
     def testHooksNotice(self):
-        self.msg = Message(":GnoXter!gnoxter@local-954F9AB.clients.your-server.de NOTICE #chan :Momentan blocken sie das Pluginsystem  bzw. update()")
-        self.plugins.process(self.msg)
-        self.assertEqual(self.msg, self.testPlugin.getLastMessage())
+        msg = Message(":GnoXter!gnoxter@local-954F9AB.clients.your-server.de NOTICE #chan :Momentan blocken sie das Pluginsystem  bzw. update()")
+        self.plugins.process(msg)
+        self.assertEqual(msg, self.testPlugin.getLastMessage())
         
     def testHooksRaw(self):
-        self.msg = Message(":localhost.local 366 Lilaine #wifi :End of /NAMES list.")
-        self.plugins.process(self.msg)
-        self.assertEqual(self.msg, self.testPlugin.getLastMessage())
+        msg = Message(":localhost.local 366 Lilaine #wifi :End of /NAMES list.")
+        self.plugins.process(msg)
+        self.assertEqual(msg, self.testPlugin.getLastMessage())
+        
+    def testHooksCTCP(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net PRIVMSG Lilaine :VERSION")
+        self.plugins.process(msg)
+        self.assertEqual(msg, self.testPlugin.getLastMessage())
+        
+    def testHooksJoin(self):
+        msg = Message(":Lilaine!lilaine@A7C823CA.E483845.77FD6974.IP JOIN :#channel")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getTarget()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])
+            
+    def testHooksPart(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net PART #channel :test")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getTarget(), msg.getMessage()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])
+            
+    def testHooksQuit(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net QUIT :test")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getMessage()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])        
+            
+    def testHooksTopic(self):
+        msg = Message(":hexa-!martin@lounge-FE1E1050.dip.t-dialin.net TOPIC #channel :sample message")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getTarget(), msg.getMessage()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])        
+            
+    def testHooksNick(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net NICK :hexaa")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getMessage()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])
+            
+    def testHooksMode(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net MODE #channel +b test!*@*")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getTarget(), msg.getMode()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])  
+            
+    def testHooksUserMode(self):
+        msg = Message(":Lilaine MODE Lilaine :+iwxz")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getNick(), msg.getTarget(), msg.getMode()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])  
+            
+    def testHooksKick(self):
+        msg = Message(":hexa-!hexa@local-FE1E1050.dip.t-dialin.net KICK #chan carliii :kickmessage")
+        self.plugins.process(msg)
+        response = self.testPlugin.getLastMessage()
+        expect = [msg.getUser(), msg.getTarget(), msg.getVictim(), msg.getMessage()]
+        for i in range(0, len(expect)):
+            self.assertEqual(response[i], expect[i])  
+            
         
 def suite():
     suite = unittest.TestSuite()
