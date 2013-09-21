5cd3ffe3651187624829955c77279425e6399a61
hexa- <mweinelt@gmail.com>
2013-09-21 02:56:58 +0200

    Lib/IRC: Update string formatting, remove some clutter

--- lib/IRC.py
+++ lib/IRC.py
@@ -4,7 +4,7 @@ from .EventHandler import EventHandler
 
 class IRC(object):
     def __init__(self, with_tls):
-        self.kill_received = False
+        self.killed = False
         self.lin =  []
         self.lout = []
         self.events = EventHandler()
@@ -15,7 +15,7 @@ class IRC(object):
         self.ping_message = "keep-alive"
         # settings
         self.with_tls = with_tls
-        self.reconnect_delay = 10
+        self.reconnect_delay = 60
         # state
         self.connected = False
         self.connecting = False
@@ -26,7 +26,7 @@ class IRC(object):
            
     def run(self):
         buf = ""
-        while (not self.kill_received):
+        while (not self.killed):
             ready_to_read, ready_to_write, in_error = select.select([self.handle],[],[], 0.5)
             if len(ready_to_read) == 1:
                 buf += self.get()       # just append, as we don't know where the newlines are
@@ -35,12 +35,12 @@ class IRC(object):
 
                 # loop over incoming lines
                 for line in lines:
-                    print("<- %s" % line)
+                    print("<- {}".format(line))
                     tok = line.split()
 
                     # PING request
                     if (tok[0] == "PING"):
-                        self.put("PONG %s" % tok[1])
+                        self.put("PONG {msg}".format(msg=tok[1]))
                     # PONG answer
                     elif (tok[1] == "PONG"):
                         if (tok[3].endswith(self.ping_message)):
@@ -58,6 +58,7 @@ class IRC(object):
                         self.put(qout.get())
                         i += 1
         print("Disconnecting...")
+        self.put("QUIT :Killed")
         self.handle.close()
         
     def update(self, diff):
@@ -70,19 +71,15 @@ class IRC(object):
                 
             if (event.getName() == "timeout"):
                 if (self.connected):
-                    self.put("PING :%s" % self.ping_message)
+                    self.put("PING :{msg}".format(msg=self.ping_message))
                     self.events.register("reconnect", self.max_ping_delay * 1000)
             elif (event.getName() == "reconnect"):
                 self.reconnect()
 
     def kill(self):
-        if (self.connected):
-            self.put("QUIT :SIGINT")
+        if (self.connecting and not self.connected):
             self.handle.close()
-        self.kill_received = True
-
-    def killed(self):
-        return self.kill_received
+        self.killed = True
 
     def connect(self, host, port, nickname, ident, realname, password = None):
         self.connected = False
@@ -109,20 +106,20 @@ class IRC(object):
         # and connect
         self.connecting = True
         try:
-            print("Connecting to %s:%d..." % (sockaddr[0], sockaddr[1]))
+            print("Connecting to {host}:{port}...".format(host=sockaddr[0], port=sockaddr[1]))
             self.handle.connect(sockaddr)
         except socket.error as err:
-            print("Unable to connect: %s" % err)
-            print("Retrying in %ss..." % self.reconnect_delay)
+            print("Unable to connect: {error}".format(error=err))
+            print("Retrying in {delay}s...".format(delay=self.reconnect_delay))
             self.events.register("reconnect", self.reconnect_delay * 1000)
             return
         
         # authentication
         if (password != None):
-            self.put("PASSWORD %s" % password)
+            self.put("PASSWORD {}".format(password))
             
-        self.put("NICK %s" % nickname)
-        self.put("USER %s %s pyLilaine :%s" % (ident, ident, realname))
+        self.put("NICK {nick}".format(nick=nickname))
+        self.put("USER {ident} {mode} {hostname} :{realname}".format(ident=ident, mode=0, hostname="*", realname=realname))
         
         self.connecting = False
         self.connected = True
@@ -133,14 +130,16 @@ class IRC(object):
         in case we get disconnected destroy the socket
         and reconnect
         '''
+        if (self.killed):
+            return
         self.handle.close()
         self.connecting = True
         self.connect(self.host, self.port, self.nickname, self.ident, self.realname, self.password)
             
     def put(self, msg):
         try:
-            print("-> %s" % msg)
-            self.handle.send(bytes("%s\n\n" % msg, 'UTF-8'))
+            print("-> {}".format(msg))
+            self.handle.send(bytes("{}\n\n".format(msg), 'UTF-8'))
         except socket.error:
             if (not self.connecting):
                 self.reconnect()
